e5fb38d832f3ff8966c9b5a41f4d17ef084c7934
FyuroTEX <yurilev4@gmail.com>
2020-06-26 02:40:22 +0300

    add links logic

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,15 +1,15 @@
 import React, { useContext } from 'react';
-import { Link, NavLink, useHistory } from 'react-router-dom';
+import { Link, NavLink} from 'react-router-dom';
 import { AuthContext } from './../context/AuthContext';
 
 
 export const Navbar = () => {
-    const history = useHistory();
+    // const history = useHistory();
     const auth = useContext(AuthContext);
-    const logoutHandler = () => {
-        auth.logout();
-        history.push('/');
-    };
+    // const logoutHandler = () => {
+    //     auth.logout();
+    //     history.push('/');
+    // };
     return (
         <nav>
             <div className="nav-wrapper light-blue darken-1" style={{padding: '0 2rem'}}>
@@ -17,7 +17,7 @@ export const Navbar = () => {
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
                     <li><NavLink to="/create">Create</NavLink></li>
                     <li><NavLink to="/links">Links</NavLink></li>
-                    <li><Link to='/' onClick={logoutHandler} className="waves-effect waves-light btn">Logout</Link></li>
+                    <li><Link to='/' onClick={auth.logout} className="waves-effect waves-light btn">Logout</Link></li>
                 </ul>
             </div>
         </nav>
--- models/User.js
+++ models/User.js
@@ -4,6 +4,6 @@ const schema = new Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     links: [{ type: Types.ObjectId, ref: 'Link' }]
-})
+});
 
 module.exports = model('User', schema);
